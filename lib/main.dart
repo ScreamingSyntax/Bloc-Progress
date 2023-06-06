8e61f9bb539516e3941adb33e4d4a3c49a10f0af
Aaryan Jha <whcloud91@gmail.com>
2023-06-06 10:46:53 +0545

    Task3 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:bloc/bloc.dart';
-class CounterCubit extends Cubit<int>{
-  CounterCubit():super(0);
-  void increment()=>emit(state+1);
-  void decrement()=> emit(state-1);
 
+enum CounterEvent{increment,decrement}
+
+class CounterBloc extends Bloc<CounterEvent,int>{
+  CounterBloc() : super(0);
+  // void increment()=> emit(state+1);
+  // void decrement()=> emit(state-1);
+
+  Stream<int> mapEventToState(CounterEvent event) async*{
+    switch(event){
+      case CounterEvent.increment:
+      yield state+1;
+      break;
+      case CounterEvent.decrement:
+      yield state-1;
+      break;
+    }
+    throw UnimplementedError();
+  }
+  
 }
 
-void main(List<String> args){
-  final cubit = CounterCubit();
-  print(cubit.state);
-  cubit.increment();
-  print(cubit.state);
-  cubit.decrement();
-  print(cubit.state);
-  cubit.increment();
-  print(cubit.state);
+Future<void> main(List<String> args) async{
+  final bloc = CounterBloc();
+  // final streamSubscription = bloc.listen();
+  bloc.add(CounterEvent.increment);
+  await Future.delayed(Duration.zero);
+  await bloc.close(); 
 }
