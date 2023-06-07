38d7cf3221cb302e441d5586a1ea5f581f321056
Aaryan Jha <whcloud91@gmail.com>
2023-06-07 19:10:44 +0545

    Task 4, Counter Cubit Made

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -2,7 +2,9 @@ part of 'counter_cubit.dart';
 
 class CounterState{
   int counterValue;
+  bool wasIncremented;
   CounterState({
     required this.counterValue,
+    required this.wasIncremented
   });
 }
