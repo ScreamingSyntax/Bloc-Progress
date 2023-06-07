6aa5504688d8fc08c1d1bf7a69956dffcbfd1669
Aaryan Jha <whcloud91@gmail.com>
2023-06-07 17:33:54 +0545

    Task 4, Counter Cubit Made

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,76 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:day1/cubit/counter_cubit.dart';
 
-enum CounterEvent{increment,decrement}
+void main() => runApp(MyApp());
 
-class CounterBloc extends Bloc<CounterEvent,int>{
-  CounterBloc() : super(0);
-  // void increment()=> emit(state+1);
-  // void decrement()=> emit(state-1);
-
-  Stream<int> mapEventToState(CounterEvent event) async*{
-    switch(event){
-      case CounterEvent.increment:
-      yield state+1;
-      break;
-      case CounterEvent.decrement:
-      yield state-1;
-      break;
-    }
-    throw UnimplementedError();
-  }
-  
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-Future<void> main(List<String> args) async{
-  final bloc = CounterBloc();
-  // final streamSubscription = bloc.listen();
-  bloc.add(CounterEvent.increment);
-  bloc.add(CounterEvent.increment);
-  bloc.add(CounterEvent.decrement);
-  bloc.add(CounterEvent.decrement);
-  await Future.delayed(Duration.zero);
-  await bloc.close(); 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You have pressed this button for this many times"),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                if(state.counterValue<0){
+                  return Text(
+                    "The counter value is negative"
+                  );
+                }
+                if(state.counterValue%2==0){
+                  return Text("The counter value ${state.counterValue} is divisible by 2");
+                }
+                return Text(
+                  "Hello ${state.counterValue}",
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  tooltip: 'Decrement',
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  tooltip: 'Increment ',
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
