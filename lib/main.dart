a0f5d6a9f1461ea9c92337545f4645bf0326d9e6
Aaryan Jha <whcloud91@gmail.com>
2023-06-13 20:10:22 +0545

    Global Access

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // import 'package:day1/presentation/pages/home_page.dart';
+import 'package:day1/logic/cubit/CounterCubit.dart';
 import 'package:day1/presentation/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,9 +14,12 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // onGenerateInitialRoutes: (initialRoute) ,
-      onGenerateRoute: _appRouter.onGenegerateRoute,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        // onGenerateInitialRoutes: (initialRoute) ,
+        onGenerateRoute: _appRouter.onGenegerateRoute,
+      ),
     );
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -4,31 +4,19 @@ import 'package:day1/presentation/pages/second_page.dart';
 import 'package:day1/presentation/pages/third_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
-  CounterCubit counterCubit = CounterCubit();
   Route? onGenegerateRoute(RouteSettings? routeSettings) {
     switch (routeSettings!.name) {
       case '/':
-        return MaterialPageRoute(
-            builder: (_) =>
-                BlocProvider.value(value: counterCubit, child: HomePage()));
+        return MaterialPageRoute(builder: (_) => HomePage());
       case '/second':
-        return MaterialPageRoute(
-            builder: (_) =>
-                BlocProvider.value(value: counterCubit, child: SecondPage()));
+        return MaterialPageRoute(builder: (_) => SecondPage());
 
       case '/third':
-        return MaterialPageRoute(
-            builder: (_) =>
-                BlocProvider.value(value: counterCubit, child: ThirdPage()));
+        return MaterialPageRoute(builder: (_) => ThirdPage());
       default:
         return null;
     }
-  }
-
-  void disclose() {
-    counterCubit.close();
   }
 }
